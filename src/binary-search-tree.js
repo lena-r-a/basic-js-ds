59151ab40bf836584b50b3bf572a8764dcd1d30c
lena-r-a <rizhankova.elena@gmail.com>
2021-10-15 19:26:59 +0300

    add: first 2 in binary

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,33 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 module.exports = class BinarySearchTree {
-
+  constructor () {
+    this.first=null;
+  }
+ 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.first;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data);
+    if (!this.first) {this.first = newNode;}
+    else addNewNode(this.root,newNode);
+
+    function addNewNode(currentNode, addNode) {
+        if (currentNode.data<addNode.first) {
+          if (!currentNode.right) {currentNode.right=addNode} else {addNewNode(currentNode.right,addNode)}
+        }
+        else {
+          if(!currentNode.first) {currentNode.left=addNode} else {addNewNode(currentNode.left,addNode)}
+        }
+    }
   }
 
   has(/* data */) {
