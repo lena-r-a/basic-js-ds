4757ae22d88368844254171a4a8bfbc35142a643
lena-r-a <rizhankova.elena@gmail.com>
2021-10-18 15:22:44 +0300

    feat: add some methods for bst

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -17,22 +17,33 @@ module.exports = class BinarySearchTree {
 
   add(data) {
     let newNode = new Node(data);
-    if (!this.first) {this.first = newNode;}
-    else addNewNode(this.root,newNode);
-
-    function addNewNode(currentNode, addNode) {
-        if (currentNode.data<addNode.first) {
-          if (!currentNode.right) {currentNode.right=addNode} else {addNewNode(currentNode.right,addNode)}
-        }
-        else {
-          if(!currentNode.first) {currentNode.left=addNode} else {addNewNode(currentNode.left,addNode)}
-        }
-    }
+    if (this.first===null) {this.first = newNode;}
+    else {this.addNewNode(this.first,newNode)};
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addNewNode(currentNode, addNode) {
+    if (currentNode.data<addNode.data) {
+      if (currentNode.right===null) {currentNode.right=addNode} else {this.addNewNode(currentNode.right,addNode)}
+    }
+    else {
+      if(currentNode.left===null) {currentNode.left=addNode} else {this.addNewNode(currentNode.left,addNode)}
+    }
+}
+  has(data) {
+    if (this.first==null) return false;
+    if (this.first.data==data) {return true}
+    else return hasData(this.first, data)
+
+    function hasData (currentNode,searchnode) {
+        if (currentNode.data>searchnode) {
+          if (currentNode.left==null) {return false}
+          if(currentNode.left.data==searchnode){return true} else {return hasData(currentNode.left,searchnode)}
+        }
+        else {
+          if (currentNode.right==null) {return false}
+          if(currentNode.right.data==searchnode){return true} else {return hasData(currentNode.right,searchnode)}
+        }
+    }
   }
 
   find(/* data */) {
@@ -46,13 +57,19 @@ module.exports = class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.first) {return null} else {return getMin(this.first)}
+    function getMin(node) {
+      if (node.left===null) {return node.data}
+      else {return getMin(node.left)}
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.first) {return null} else {return getMax(this.first)}
+    function getMax(node) {
+      if (node.right===null) {return node.data}
+      else {return getMax(node.right)}
+    }
   }
 
 }
